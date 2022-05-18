0abfa9287240a1ae68191f5ce62ddd6575c31afa
mdsafiulazam <mdriadkhan20@gmail.com>
2022-05-19 03:29:29 +0600

    added post delete method

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000
-
+const port = process.env.PORT || 5002
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://userDB:<password>@cluster0.6m4yg.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6m4yg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect()
+        const todoTaskCollection = client.db('todoTaskList').collection('taskList')
+        
+        app.get('/todo',async(req, res)=>{
+            const todoTask = await todoTaskCollection.find().toArray()
+            res.send(todoTask)
+        })
+        app.post('/todo',async (req, res)=>{
+            const todo = req.body 
+            const result = await todoTaskCollection.insertOne(todo)
+            res.send(result)
+        })
+        app.delete('/todo/:id',async(req, res)=>{
+            const id = req.params.id 
+            const query = {_id:ObjectId(id)}
+            const result = await todoTaskCollection.deleteOne(query)
+            res.send(result)
+        })
+    
+       
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/',(req, res)=>{
+    res.send('to-do-task app is running')
+})
+app.listen(port, ()=>{
+    console.log('listing', port)
+})
